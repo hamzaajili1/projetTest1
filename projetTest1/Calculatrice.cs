9676f10db39485c59450b8b0ed457a21761aa1d7
Hamza Ajili <hamza-ajili@hotmail.com>
2024-01-31 10:31:51 +0100

    Ajoutez des fichiers projet.

---
+++ projetTest1/Calculatrice.cs
@@ -0,0 +1,10 @@
+﻿namespace projectTest1
+{
+    public class Calculatrice
+    {
+        public static int Addition(int nombre1, int nombre2)
+        {
+            return nombre1 + nombre2;
+        }
+    }
+}
---
+++ projetTest1/Program.cs
@@ -0,0 +1,23 @@
+﻿using projectTest1;
+using System;
+
+namespace projectTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez le premier nombre :");
+            int nombre1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entrez le deuxième nombre :");
+            int nombre2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultat = Calculatrice.Addition(nombre1, nombre2);
+
+            Console.WriteLine("Le résultat de l'addition de " + nombre1 + " et " + nombre2 + " est : " + resultat);
+            Console.WriteLine("Appuyez sur une touche pour fermer...");
+            Console.ReadKey();
+        }
+    }
+}
